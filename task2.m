a0a4304dfaa3813c60e3997a9a54eb0fed604990
Erling Singstad Paulsen <erling.singstad.paulsen@gmail.com>
2014-11-04 13:48:27 +0100

    task2

--- task2.m
+++ task2.m
@@ -1,6 +1,25 @@
-function [ output_args ] = task2( input_args )
-%TASK2 Summary of this function goes here
-%   Detailed explanation goes here
+function task2(filename)
+    file = strcat(filename, '.txt');
+    
+    fileID = fopen(file, 'r');
+    
+    [Pvector, count] = fscanf(fileID, ['%d' '%d' '%d' '%d' '%d' '%d' '%d' '%d' ';']);
+    
+    Points = zeros((count/8), 8);
+    k = 1;
+    
+    for i = 1 : count/8
+        for j = 1 : 8
+            Points(i, j) = Pvector(k);
+            k = k + 1;
+        end
+    end
+    
+    for i = 1 : length(Points(:, 1))
+        
+    end
+
+    
 
 
 end
