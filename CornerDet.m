085c0292d15cdbc38e3218ce6d55e265ddece389
Erling Singstad Paulsen <erling.singstad.paulsen@gmail.com>
2014-11-04 14:52:26 +0100

    Stuff

---
+++ CornerDet.m
@@ -0,0 +1,82 @@
+function C = CornerDet(X)
+% CornerDet detects corners of traced outlines using the SAM04 algorithm. 
+% The outline is assumed to constitute a discrete closed curve where each
+% point is included just once.
+% 
+%
+% INPUT: X (A traced outline forming a discrete closed curve)
+%
+% OUTPUT: C (The indices of X that consitute corner points)
+%
+
+n = length(X);
+
+% Sets the parameters of the algorithm. Increasing D and R will give the
+% same number of corners or fewer corners.
+L = 80; % Controls the scale at which corners are measured.
+R = 30; % Controls how close corners can appear.
+D = 14; % The lower bound for the corner metric. Corner candidates with 
+        % lower metric than this are rejected.
+
+        
+% Finds corner candidates        
+d = zeros(1,n);
+for i = 1:n
+    if i+L <= n
+        k = i+L;
+        index = i+1:k-1;
+    else
+        k = i+L-n;
+        index = [i+1:n,1:k-1];
+    end
+    
+    M = X(:,k)-X(:,i);
+    
+    if M(1) == 0
+        dCand = abs(X(1,index)-X(1,i));
+    else
+        m = M(2)/M(1);
+        dCand = abs(X(2,index)-m*X(1,index)+m*X(1,i)-X(2,i))/sqrt(m^2+1);
+    end
+    
+    [Y,I] = max(dCand);
+    if Y > d(index(I))
+        d(index(I)) = Y;
+    end
+end
+
+% Rejects candidates which do not meet the lower metric bound D.
+index = d < D;
+d(index) = 0;
+C = 1:n;
+index = ~logical(index);
+C = C(index);
+
+
+% Rejects corners that are too close to a corner with larger metric.
+l = length(C);
+j = 1;
+while j < l
+    if abs(C(j)-C(j+1)) <= R
+        if d(C(j)) > d(C(j+1))
+            C = C([1:j,j+2:l]);
+        else
+            C = C([1:j-1,j+1:l]);
+        end
+        l = l-1;
+    else
+        j = j+1;
+    end
+end
+
+if l > 1 && abs(C(1)+n-C(end)) <=R
+    if d(C(end)) > d(C(1))
+        C = C(2:end);
+    else
+        C = C(1:end-1);
+    end
+end
+
+
+end
+
---
+++ genOutline.m
@@ -0,0 +1,57 @@
+% The script genOutline generates a traced outline of a bitmap image.
+% The image is assumed to consist of a single object whose outline is
+% desired.
+
+I = imread('batman.jpg'); % Read the image file. image(I) displays the image.
+
+BW = im2bw(I); % Converts the image to a black and white image. Can display
+               % the image using imshow(BW);
+
+BW = ~BW; % Switches black and white. Skip if your image has a black background.
+
+BW = imfill(BW,'holes'); % Fills any holes in the image.
+
+BW1 = bwperim(BW); % Detects the perimeter/outline of the image.
+
+[I,J] = find(BW1,1,'first'); % Finds the starting point for the tracing algorithm.
+
+X = bwtraceboundary(BW1,[I,J],'W')'; % Traces the image clockwise.
+
+% Transform from row and column coordinates to traditional x and y;
+X = [X(2,:);-X(1,:)]; 
+m = min(X(2,:));
+X(2,:) = X(2,:) - m; 
+
+
+% Check if the outline is a closed discrete curve.
+if norm(X(:,1)-X(:,end),inf) < eps
+    X = X(:,1:end-1); % Remove the last point so every point is included 
+                      % just once.
+else
+    error('The outline is not a closed discrete curve');
+end
+    
+
+s = 2; % Smoothing parameter. Higher value results in more smoothing.
+
+% Smooths the outline.
+Xc = X;
+for i = 1:s
+X = X + Xc(:,[1+i:end,1:i]);
+X = X + Xc(:,[end-(i-1):end,1:end-i]);
+end
+X = X/(2*s+1);
+
+% Finds the corner indices of the image. It may be necessary to adjust the
+% parameters of the algorithm to get a satisfactory result.
+C = CornerDet(X);
+
+
+% Plots the discrete curve.
+plot(X(1,[1:end,1]),X(2,[1:end,1]),'k-','LineWidth',1);
+axis equal;
+
+% Overlays the found corner points.
+hold on;
+plot(X(1,C),X(2,C),'r*','MarkerSize',8);
+hold off;
