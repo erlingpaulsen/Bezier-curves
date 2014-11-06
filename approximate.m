583bbea85d79ef1f6f15177cf033453f1b67f310
andersgg <anders>
2014-11-06 12:25:01 +0100

    la inn discurve og div

---
+++ approximate.m
@@ -0,0 +1,14 @@
+function svar =approximate(x,y)
+    
+    M = [-1 3 -3 1; 3 -6 3 0; -3 3 0 0; 1 0 0 0];
+    T = [t^3, t^2, t, 0];
+    C = [c1, c2 , c3, c4];
+    
+    d=zeros(1;
+    
+    
+    
+    
+    svar = 1;
+end
+    
