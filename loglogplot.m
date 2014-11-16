1c38d2617a6a8cb6aebd9e50b5ac345ae144247f
larsbow <larsbow@stud.ntnu.no>
2014-11-16 14:46:32 +0100

    Loglogplot minor change

--- loglogplot.m
+++ loglogplot.m
@@ -1,14 +1,28 @@
-function loglogplot(n);
+function loglogplot();
 x = linspace(0,1,101);
 y = sin(pi*x);
 ys = [];
 xs=[];
-for i = 0:n
-    list = splitbezier2(x, y, 0, 0, (ceil(101/(2^i)))+1);
-    ys = [ys, sum(list)/length(list)];
-    xs = [xs, length(list)];
-end
+list = splitbezier2(x, y, 0, 0, 102);
+ys = [ys, sum(list)/length(list)];
+xs = [xs, length(list)];
+list = splitbezier2(x, y, 0, 0, 52);
+ys = [ys, sum(list)/length(list)];
+xs = [xs, length(list)];
+list = splitbezier2(x, y, 0, 0, 27);
+ys = [ys, sum(list)/length(list)];
+xs = [xs, length(list)];
+list = splitbezier2(x, y, 0, 0, 15);
+ys = [ys, sum(list)/length(list)];
+xs = [xs, length(list)];
+list = splitbezier2(x, y, 0, 0, 9);
+ys = [ys, sum(list)/length(list)];
+xs = [xs, length(list)];
+list = splitbezier2(x, y, 0, 0, 6);
+ys = [ys, sum(list)/length(list)];
+xs = [xs, length(list)];
 disp(xs);
+disp(ys);
 hold off;
 loglog(xs, ys);
 end
